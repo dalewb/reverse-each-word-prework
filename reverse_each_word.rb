0d9fe6dad8732686eb19993e097bf8136c9d1fa1
William Dale <williambdale@gmail.com>
2018-03-05 23:03:09 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  # result = []
+  # string.split(' ').each do |el|
+  #   result << el.reverse
+  # end 
+  # result.join(' ')
+  
+  string.split(' ').collect {|el| el.reverse}.join(' ')
+end 
